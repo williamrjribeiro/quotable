f30aea88edf035379ed1b11de6cf58b050e44ae8
William R. J. Ribeiro <Will@Williams-MacBook-Air.local>
2016-04-06 11:38:27 +0100

    Added Angular Client to the build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,22 +4,59 @@ import rimraf from 'rimraf';
 import run from 'run-sequence';
 import watch from 'gulp-watch';
 import server from 'gulp-live-server';
+import browserify from 'browserify';
+import source from "vinyl-source-stream";
 
 const PATHS = {
-    js: ['./server/**/*.js'],
-    destination: './dist'
+    server: {
+        dist:   './dist',
+        src:    './server',
+        main:   './server/index.js'
+    },
+    client: {
+        dist:   './public',
+        src:    './client',
+        main:   './client/js/app.js'
+    },
+    js: '/**/*.js',
+    html: '/**/*.html'
 };
 
 gulp.task('default', done => {
-    run('server', 'build', 'watch', done);
+    //run('server', 'build', 'watch', done);
+    run('clean',['build-server','transpile'],'copy-client','restart','watch', done);
 });
 
-gulp.task('build', done => {
-    run('clean', 'flow', 'babel', 'restart', done);
+gulp.task('build-server', done => {
+    run('flow', 'babel', 'server', done);
+});
+
+gulp.task('transpile', () => {
+    return browserify(PATHS.client.main)
+        .transform("babelify")
+        .bundle()
+        .on("error", function (error) {
+            console.error( `\n[gulp.transpile] Error: ${error.message}\n`);
+            this.emit("end");
+        })
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest(PATHS.client.dist));
+});
+
+gulp.task('copy-client', () => {
+    gulp.src(PATHS.client.src + PATHS.html).pipe(gulp.dest(PATHS.client.dist));
 });
 
 gulp.task('clean', done => {
-    rimraf(PATHS.destination, done);
+    run(['clean-server','clean-client'], done);
+});
+
+gulp.task('clean-server', done => {
+    rimraf(PATHS.server.dist, done);
+});
+gulp.task('clean-client', done => {
+
+    rimraf(PATHS.client.dist, done);
 });
 
 gulp.task('flow', shell.task([
@@ -28,20 +65,28 @@ gulp.task('flow', shell.task([
 
 gulp.task('babel', shell.task([
     'babel server --out-dir dist'
+    //`babel ${PATHS.server.src} --out-dir ${PATHS.server.dist}`
 ], {ignoreErrors: true}));
 
 let express;
 
 gulp.task('server', () => {
-    express = server.new(PATHS.destination);
+    express = server.new(PATHS.server.dist);
 });
 
 gulp.task('restart', () => {
-    express.start.bind(express)();
+    if(express)
+        express.start.bind(express)();
+    else {
+        console.log("[gulp.restart] Express server don't exist. Continue...");
+    }
 });
 
 gulp.task('watch', () => {
-    return watch(PATHS.js, () => {
+    watch(PATHS.server.src + PATHS.js, () => {
         gulp.start('build');
+    });
+    watch(PATHS.client.src + PATHS.js, () => {
+        gulp.start('transpile');
     });
 });
--- server/index.js
+++ server/index.js
@@ -11,6 +11,54 @@ app.use(express.static(__dirname + '/../public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", (req, res) => {
-    res.send("Hello Quotable! BITCHES! \n");
-}).listen(3000, () => console.log("Express Server listening on port 3000"));
+
+let coreQuote = {
+    name: "Jaron Lanier",
+    text: "To be a person you have to find a sweet spot in which you both invent yourself and are real",
+    source: "You Are Not A Gadget",
+    picture: "url.jpg"
+};
+let source = {
+    id: "s1",
+    title: "You Are Not A Gadget",
+    type: "book",
+    quotes: [
+        {
+            id: "t1",
+            text: "To be a person you have to find a sweet spot in which you both invent yourself and are real.",
+            location: "3369-3372",
+            tags: ["reality","person"]
+        },
+        {
+            id: "t2",
+            text: "What makes something trully real is that it is impossible to represent it to completion.",
+            location: "2450-2451",
+            tags: ["reality"]
+        },
+        {
+            id: "t3",
+            text: "'Troll' is a term for an anonymous person who is abusive in an online environment.",
+            location: "1180-1181",
+            tags: ["internet","definition"]
+        }
+    ]
+};
+let author = {
+    id: "a1",
+    name: "Jaron Lanier",
+    sources: [
+        source
+    ]
+};
+
+app.route('/authors')
+.get((res, req, next) => {
+
+});
+
+app.route('/sources')
+.get((res, req, next) => {
+
+});
+
+app.listen(3000, () => console.log("Express Server listening on port 3000"));
